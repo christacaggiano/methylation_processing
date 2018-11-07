ca2351e354e1e64634b4422ba5167517b08a891b
christacaggiano <christacaggiano@gmail.com>
2018-11-06 18:33:19 -0800

    some random data

--- sum_region.py
+++ sum_region.py
@@ -1,37 +1,44 @@
 import csv
 
 def get_coords(line): 
+	return str(line[0]), int(line[1]), int(line[2])
 
-	return int(line[1]), int(line[2])
+def sum_counts(region): 
+	counts = []
+	for cpg in region: 
+		counts.append([int(cpg[count]) for count in range(3, len(cpg), 2)])
+	return [sum(x) for x in zip(*counts)]
+
+def write_summed_counts(summed_counts, chrom, beginning, end, output): 
+	output.writerow([chrom] + [beginning] + [end] + summed_counts)
 
 if __name__ == "__main__": 
 
 	window_size = 10
-	file_name = "test/test_set_sample.txt"
+	file_name = "data/test_set_sample.txt"
+	output_file = "data/test_summed.txt"
 	
-	with open(file_name) as f: 
+	with open(file_name, "r") as input_file, open(output_file, "w") as output_file: 
 		
-		bed_file = csv.reader(f, delimiter='\t')
+		bed_file = csv.reader(input_file, delimiter='\t')
+		summed_file = csv.writer(output_file, delimiter='\t')
 
 		line = next(bed_file)
-		beginning, end = get_coords(line)
+		previous_chrom, beginning, end = get_coords(line)
 		region = [line]
 
 		for line in bed_file: 
-			# print(end - beginning)
-			print("beginning: " + str(beginning))
-			print("end: " + str(end))
-			if end - beginning <= window_size: 
-				region.append(line)
-				region.append(["A"])
-				_, end = get_coords(line)
-			else: 
-				# print(region)
-				region = [line] 
-				region.append(["E"])
-				beginning, end = get_coords(line)
-			print(region)
+		
+			chrom, _, end = get_coords(line)
 
+			if end - beginning <= window_size and chrom==previous_chrom: 
+				region.append(line)
+				previous_chrom = chrom
+			else: 
+				summed_counts = sum_counts(region)
+				write_summed_counts(summed_counts, chrom, beginning, end, summed_file)
+				region = [line] 
+				chrom, beginning, _ = get_coords(line)
 
 
 
