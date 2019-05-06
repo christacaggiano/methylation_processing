973b65a2bacedf4c3b712fc8c56ca733c6b52118
christacaggiano <christacaggiano@gmail.com>
2019-05-06 14:25:36 -0700

    semi finished version of sum by list

--- sum_by_list.py
+++ sum_by_list.py
@@ -7,32 +7,51 @@ import collections
 
 def get_region_dict(file): 
 
-	regions = defaultdict(list)
+	regions_dict = collections.defaultdict(list)
 
 	with open(file, "r") as input_file: 
 		list_file = csv.reader(input_file, delimiter="\t")
 
 		for line in list_file: 
 			chrom, start, end = line[0], line[1], line[2]
-			regions[chrom].append({"start":start, "end":end, "meth":0, "unmeth":0})
+			regions_dict[chrom].append({"start":int(start), "end":int(end), "meth":0, "unmeth":0, "total_cpgs":0})
 
-	return regions
+	return regions_dict
+
+
+def get_methylation_counts(file, regions_dict): 
+	with open(file, "r") as input_file: 
+		cpg_file = csv.reader(input_file, delimiter="\t")
+
+		for line in cpg_file: 
+			chrom, start, end, meth, unmeth = line 
+			
+			if chrom in regions_dict: 
+				for region in regions_dict[chrom]: 
+					if int(start) >= region["start"] and int(end) <= region["end"]: 
+						region["meth"] += float(meth)
+						region["unmeth"] += float(unmeth)
+						region["total_cpgs"] += 1
+
+	return regions_dict		
+
+
+def write_bed_file(output_file, regions_dict): 
+	with open(output_file, "w") as output: 
+		bed_file = csv.writer(output, delimiter="\t")
+
+		for chrom in regions_dict: 
+			for region in regions_dict[chrom]:
+				bed_file.writerow([chrom], [region["start"]], [region["end"]], [region["meth"]], [region["unmeth"]])
 		
 		
 if __name__ == "__main__": 
 
 	list_file = sys.argv[1]
-	input_file_name = sys.argv[2]
+	tissue_cpg_file = sys.argv[2]
 	output_file_name = sys.argv[3]
 	
-	print(get_region_dict(file))
-
-	# with open(list_file, "r") as lf, open(input_file_name, "r") as i, open(output_file_name, "w") as o: 
-		
-	# 	list_file = csv.reader(lf, delimiter='\t')
-	# 	input_file = csv.writer(i, delimiter='\t')
-
-	# 	output_file = csv.writer(o, delimiter='\t')
-
-
+	regions = get_region_dict(list_file)
+	get_methylation_counts(tissue_cpg_file, regions)
+	write_bed_file(output_file_name, regions)
 
